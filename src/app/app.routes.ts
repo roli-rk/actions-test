fa35161b67c32d80071d9e70ed81a2334d478051
Roland Roßkopf <roli.rossi@hotmail.com>
2024-12-15 18:04:13 +0100

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    {path: "home", component: AppComponent}, 
+    {path: "", component: AppComponent}, 
+    {path:"**", redirectTo:"", pathMatch:"full"}
 ];
