765cb36ebf6abf8247086e4fc8e0afbd52e23889
Roland Roßkopf <roli.rossi@hotmail.com>
2024-12-15 17:56:23 +0100

    remove dist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,4 @@ import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {path: "home", component: AppComponent}, 
-    {path: "", redirectTo:"home", pathMatch:"full"},
-    {path:"**", redirectTo:"home", pathMatch:"full"}
 ];
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import { provideStoreDevtools } from '@ngrx/store-devtools';
 // Importiere deine Reducer und Effects
 import { TasksEffects } from './app/state/tasks.effects';
 import { tasksReducer } from './app/state/tasks.reducer';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(routes),
     provideStore({ tasks: tasksReducer }),
     provideEffects([TasksEffects]),
     provideStoreDevtools()
